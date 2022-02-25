75237a734a25f01c11da370c9617ea53939237f9
masjohncook <mas.john.cook@gmail.com>
2022-02-25 11:12:25 +0800

    feat - update the local_slowloris.py

--- local_slowloris.py
+++ local_slowloris.py
@@ -7,7 +7,7 @@ import sys
 
 
 # HTTP Request messages
-header = [
+headers = [
     "User-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
     "Accept-language: en-US,en"
 ]
